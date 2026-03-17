63038d854ceb275a0a9e4ed83d531ef130ba4e3a
mikeblk01 <135442380+mikeblk01@users.noreply.github.com>
2026-03-16 18:37:16 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,88 @@
+let channels = [];
+
+function parseM3U(){
+
+let text = document.getElementById("input").value;
+let lines = text.split("\n");
+
+channels = [];
+
+for(let i=0;i<lines.length;i++){
+
+if(lines[i].startsWith("#EXTINF")){
+
+let info = lines[i];
+let url = lines[i+1];
+
+let name = info.split(",")[1];
+
+let groupMatch = info.match(/group-title="([^"]+)"/);
+let idMatch = info.match(/tvg-id="([^"]+)"/);
+
+let group = groupMatch ? groupMatch[1] : "";
+let epgid = idMatch ? idMatch[1] : "";
+
+channels.push({
+name:name,
+epgid:epgid,
+group:group,
+url:url
+});
+
+}
+
+}
+
+renderTable();
+
+}
+
+function renderTable(){
+
+let table = document.querySelector("#channels tbody");
+table.innerHTML="";
+
+channels.forEach((c,i)=>{
+
+let row = `
+<tr>
+<td contenteditable="true">${c.name}</td>
+<td contenteditable="true">${c.epgid}</td>
+<td contenteditable="true">${c.group}</td>
+<td>${c.url}</td>
+</tr>
+`;
+
+table.innerHTML += row;
+
+});
+
+}
+
+function downloadM3U(){
+
+let rows = document.querySelectorAll("#channels tbody tr");
+
+let output="#EXTM3U\n";
+
+rows.forEach(row=>{
+
+let name=row.children[0].innerText;
+let epgid=row.children[1].innerText;
+let group=row.children[2].innerText;
+let url=row.children[3].innerText;
+
+output+=`#EXTINF:-1 tvg-id="${epgid}" group-title="${group}",${name}\n`;
+output+=`${url}\n`;
+
+});
+
+let blob=new Blob([output],{type:"text/plain"});
+
+let a=document.createElement("a");
+
+a.href=URL.createObjectURL(blob);
+a.download="playlist.m3u";
+a.click();
+
+}
